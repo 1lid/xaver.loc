255fd5de9fab9ec99d643003beeb03f0a254edb7
master <master@master-System-Product-Name>
2015-03-16 11:49:35 +0800

    first commit

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
-<html><body><h1>KOLOBOK2</h1>
-<p>This is the default web page for this server.</p>
-<p>The web server software is running but no content has been added, yet.</p>
-</body></html>
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
